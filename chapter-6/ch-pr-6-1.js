b730346c560e5af584bfaefda5a098008169a8db
Michael <mikhail6886@yandex.ru>
2024-01-15 06:54:37 +0500

    add practical lesson 6.1 solution

--- chapter-6/ch-pr-6-1.js
+++ chapter-6/ch-pr-6-1.js
@@ -1,1 +1,7 @@
-one
+function adder(a, b) {
+	return a + b;
+}
+const val1 = Number(prompt("Enter random number"));
+const val2 = Number(prompt("Enter random number"));
+
+console.log(adder(val1, val2));
