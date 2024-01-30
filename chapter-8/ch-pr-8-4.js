83dc884edcd8640ceccb25116b82f2ca8dae97a3
Michael <mikhail6886@yandex.ru>
2024-01-30 14:56:50 +0500

    added the solution of practical lesson 8.4

---
+++ chapter-8/ch-pr-8-4.js
@@ -0,0 +1,13 @@
+const val = "thIs will be capiTalized for each word This Will Be Capitalized For Each Word.";
+
+function wordsCaps(str) {
+	str = str.toLowerCase();
+	const tempArr = [];
+	let words = str.split(" ");
+	words.forEach(word => {
+		let temp = word.slice(0, 1).toUpperCase() + word.slice(1);
+		tempArr.push(temp);
+	});
+	return tempArr.join(" ");
+}
+console.log(wordsCaps(val));
