082ffb3715ebdcb2b9980a3c3200a3eb9077026a
Michael <mikhail6886@yandex.ru>
2024-02-08 10:16:05 +0500

    added the solution of the practical lesson 9.2

---
+++ chapter-9/ch-pr-9-2.js
@@ -0,0 +1,5 @@
+//for example: open web-site https://music.yandex.ru/home
+const URL = "https://music.yandex.ru/home"
+console.dir(window);
+console.log("protocol:", "https");
+console.log("href:", URL);
