7c3aa227dde31b14f1718340ab706206643a5823
Michael <mikhail6886@yandex.ru>
2024-02-11 08:16:28 +0500

    added the solution of practical lesson 8.3

---
+++ chapter-8/ch-pr-8-3.js
@@ -0,0 +1,12 @@
+//array for experiment
+let arr = [10, 20, 30, 40, 50, 60];
+
+//the first option
+let mapped_arr1 = arr.map( function(elem) {
+	return elem * 2;
+});
+console.log(mapped_arr1);
+
+//the alternative option
+let mapped_arr2 = arr.map(x => x * 2);
+console.log(mapped_arr2);
