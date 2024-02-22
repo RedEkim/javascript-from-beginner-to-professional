34f65737d64e8d354758b881979ab96ca878b5fe
Michael <mikhail6886@yandex.ru>
2024-02-22 12:38:52 +0500

    update files of chapter 10

---
+++ chapter-10/ch-pr-10-9.js
@@ -0,0 +1,7 @@
+const btns= document.querySelectorAll("button");
+btns.forEach((btn)=>{
+    function output(){
+        console.log(this.textContent);
+    }
+    btn.addEventListener("click", output);
+});
