7c8081d38c8fa51e4879821d0ac82842859d7c36
Michael <mikhail6886@yandex.ru>
2024-02-27 12:34:30 +0500

    update of practical lesson 6.3

--- chapter-6/ch-pr-6-3.js
+++ chapter-6/ch-pr-6-3.js
@@ -1,9 +1,9 @@
 const a = Number(prompt("Enter number"));
 const b = Number(prompt("Enter number"));
-let oper = prompt("plus or minus");
+let oper = prompt("plus(+) or minus(-)?");
 
 function calculator(a, b, oper) {
-	if(oper === "plus") {
+	if(oper === "plus" || oper === "+") {
 		console.log(a + b);
 	} else {
 		console.log(a - b);
