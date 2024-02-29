be68e6c6580687b965f79f09b25ee1b8d1c7508c
Michael <mikhail6886@yandex.ru>
2024-02-29 17:10:11 +0500

    update solution of practical lesson 8.6

--- chapter-8/ch-pr-8-6.js
+++ chapter-8/ch-pr-8-6.js
@@ -4,6 +4,7 @@ console.log(PI);
 let number = 5.7;
 console.log("Ceil number:", Math.ceil(number));
 console.log("Floor number:", Math.floor(number));
+console.log("Round number:", Math.round(number));
 console.log("Random number:", Math.random());
 console.log("Random number 0 to 10:", Math.floor(Math.random() * 11)); //0-10
 console.log("Random number 0 to 100:", Math.floor(Math.random() * 100)); //0-100
