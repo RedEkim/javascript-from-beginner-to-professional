0d2447f51203938fb99e89a66760ccd2f5a95d07
Michael <mikhail6886@yandex.ru>
2024-03-01 15:40:53 +0500

    update solution of practical lesson 9.3

---
+++ chapter-9/ch-pr-9-3.js
@@ -0,0 +1,7 @@
+const output = document.querySelector(".output");
+output.textContent = "Hello World";
+output.classList.add("red");
+output.id = "tester";
+output.style.backgroundColor = "red";
+console.log(document.URL);
+output.textContent = document.URL;
